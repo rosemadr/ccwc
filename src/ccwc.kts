49197762e95c7ccffc1dc3d729e38659eacbea17
Madi Simcock-Brown <m.simcock-brown@outlook.com>
2026-05-03 14:11:08 +0100

    wip step 3

--- src/ccwc.kts
+++ src/ccwc.kts
@@ -1,22 +1,38 @@
 #!/usr/bin/env kotlin
 
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
 import kotlin.io.path.Path
 import kotlin.io.path.fileSize
 
 if (args.isNotEmpty()) {
 
-    val filename: String = args[args.lastIndex]
-
+    val filename : String = args[args.lastIndex]
     val file = File(filename)
 
     val filePath = Path(filename)
-     val lines = file.readLines()
+    val lines = file.readLines()
 
     when (args[0]) {
-        "-c" -> print("${filePath.fileSize()} $filename")
-        "-l" -> print("${lines.size} $filename")
+        "-c" -> printOutput(filePath.fileSize().toString(), filename)
+        "-l" -> printOutput(lines.size.toString() ,filename)
+        "-w" -> printOutput(findWordCount(lines).toString(), filename)
+        else -> print("invalid argument modifier")
     }
+}
+
+fun findWordCount(lines: List<String>) : Int {
+    //doesn't work, I think because whitespace strings are counted as words
+    // remove new lines,
+//    val filteredAnSplit = lines.filter { it != "" }.filterNot { is } }
+//    print(filteredAnSplit)
+//    return filteredAnSplit.sumOf {it.split(" ").trim().size}
+    return lines.filter { it != ""}.sumOf {it.split(" ").size}
+}
+
+fun printOutput(output : String, filename : String,) {
+    println("$output $filename")
 }
 
 

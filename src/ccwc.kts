638d7d97a1508bb9d214c2f0107a252397a18e56
Madi Simcock-Brown <m.simcock-brown@outlook.com>
2026-05-03 11:37:22 +0100

    completed step 1 happy path

--- src/ccwc.kts
+++ src/ccwc.kts
@@ -1,22 +1,18 @@
 #!/usr/bin/env kotlin
 
 import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.fileSize
 
 if (args.isNotEmpty()) {
+    if (args[0] == "-c") {
+        val filename: String = args[args.lastIndex]
 
+        val file = File(filename)
 
-        if (args[0].startsWith("-")) {
-            println("test")
-        }
-
-
-    val filename : String = args[args.lastIndex]
-
-    val file = File(filename)
-
-
-
-
+        val filePath = Path(filename)
+        print("${filePath.fileSize()} $filename")
+    }
 }
 
 
